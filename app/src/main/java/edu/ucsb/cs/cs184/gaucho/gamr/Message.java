2563217d3e85d493928c89a6a8f7950696cf00f6
William Bennett <williambennett2014@gmail.com>
2017-12-20 20:36:06 -0800

    Scaffolding for messaging

---
+++ app/src/main/java/edu/ucsb/cs/cs184/gaucho/gamr/Message.java
@@ -0,0 +1,23 @@
+package edu.ucsb.cs.cs184.gaucho.gamr;
+
+import java.util.Date;
+
+/**
+ * Created by william on 12/20/17.
+ */
+
+public class Message {
+    String userId;
+    String contents;
+
+    public Message() {
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+}
