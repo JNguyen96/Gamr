72aaea9fbcb14edfebaeab35e837fff19b87bc7d
Justin Nguyen <jnguy876@gmail.com>
2017-12-20 22:46:38 -0800

    UI for matches

--- app/src/main/java/edu/ucsb/cs/cs184/gaucho/gamr/MatchesActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs184/gaucho/gamr/MatchesActivity.java
@@ -3,8 +3,21 @@ package edu.ucsb.cs.cs184.gaucho.gamr;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.azoft.carousellayoutmanager.CarouselZoomPostLayoutListener;
+import com.azoft.carousellayoutmanager.CenterScrollListener;
+
+import java.util.ArrayList;
 
 /**
  * Created by Justin on 12/15/17.
@@ -16,6 +29,56 @@ public class MatchesActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matches);
+        RecyclerView rv = (RecyclerView)findViewById(R.id.recycler_view_matches);
+        LinearLayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rv.setLayoutManager(manager);
+        rv.addOnScrollListener(new CenterScrollListener());
+        rv.setAdapter(new MatchesActivity.TestAdapter(matches));
+
+    }
+
+    private static final class TestAdapter extends RecyclerView.Adapter<MatchesActivity.TestViewHolder> {
+
+        private static int mItemsCount;
+        private static ArrayList<String> matches;
+
+
+        TestAdapter(ArrayList<String> matches) {
+
+            MatchesActivity.TestAdapter.matches = matches;
+            MatchesActivity.TestAdapter.mItemsCount = matches.size();
+
+
+        }
+
+        @Override
+        public MatchesActivity.TestViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.match_view,parent,false);
+            final MatchesActivity.TestViewHolder tvh = new MatchesActivity.TestViewHolder(v);
+            return tvh;
+        }
+
+        @Override
+        public void onBindViewHolder(final MatchesActivity.TestViewHolder holder, final int position) {
+            holder.match.setText(matches.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mItemsCount;
+        }
+    }
+
+    private static class TestViewHolder extends RecyclerView.ViewHolder {
+
+        public View mView;
+        public TextView match;
+        public TestViewHolder(View view){
+            super(view);
+            mView = view;
+            match = (TextView)view.findViewById(R.id.textView);
+        }
+
     }
 
     @Override
